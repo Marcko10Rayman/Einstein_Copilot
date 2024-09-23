680484faa952f138140d46216d1ebe938cb724b2
Marco Cruz <mcruz@pkglobal.com>
2024-09-23 16:35:01 -0600

    chore(): add new class and method to the lwc

--- force-app/main/default/lwc/custom_prompt/custom_prompt.js
+++ force-app/main/default/lwc/custom_prompt/custom_prompt.js
@@ -1,16 +1,16 @@
 import { LightningElement, wire, track, api} from 'lwc';
 import getVehicleDetails from '@salesforce/apex/Vehicle_Details.getPrompt'
+import getCustomerDetails from '@salesforce/apex/Customer_Details.getPrompt'
 
 export default class Custom_prompt extends LightningElement {
 
     @api recordId;
     @track responsePrompt;
     @track onSpinner = false;
+    @track question 
 
-    handleButton() {
-        this.onSpinner = true;
-        let recordId = this.recordId;
-        console.log('RecordId: '+ recordId);
+    carDetailsMehtod(recordId) {
+        console.log('Entra al metodo de details');
         getVehicleDetails({caseId: recordId})
         .then(result => {
             let res = result;
@@ -24,6 +24,47 @@ export default class Custom_prompt extends LightningElement {
         .finally(() => {
             this.onSpinner = false;
         })
+    }
+
+    customerDetailsMehtod(recordId) {
+        getCustomerDetails({caseId: recordId})
+        .then(result => {
+            let res = result;
+            console.log(res);
+            this.responsePrompt = res;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        .finally(() => {
+            this.onSpinner = false;
+        })
+    }
+
+    get options() {
+        return [
+            { label: 'What are the client’s car details?', value: 'carDetails' },
+            { label: 'Was the client a GM customer?', value: 'customerDetails' },
+        ];
+    }
+
+    handleChange(event) {
+        this.question = event.detail.value;
+        console.log('Value of Question ===> ' + this.question);
+    }
+
+    handleButton() {
+        this.onSpinner = true;
+        let ques = this.question;
+        console.log('Question: '+ ques);
+        let recordId = this.recordId;
+        console.log('RecordId: '+ recordId);
+
+        if(ques == 'carDetails') {
+            this.carDetailsMehtod(recordId);
+        } else {
+            this.customerDetailsMehtod(recordId);
+        }
         
     }
 
